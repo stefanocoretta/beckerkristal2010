6fef40afb0a05fa61f078cfed9cc225f74d67dcf
stefanocoretta <stefano.coretta@gmail.com>
2024-03-31 14:18:18 +0100

    add wide tibble

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Becker-Kristal 2010 formant data in wide format
+#'
+#' The Becker-Kristal 2010 formant data in a wide format.
+#'
+#' @format A tibble.
+#'
+#' \describe{
+#'   \item{ISO 639-3}{Language ISO 639-3 code.}
+#'   \item{Language}{Language name.}
+#' }
+#' @source <http://phonetics.linguistics.ucla.edu/facilities/databases/databases.html>
+"beckerkristal2010_wide"
