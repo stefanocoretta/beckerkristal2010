30494ec4bdb9259c2579070c58e655e369c4e2a9
stefanocoretta <stefano.coretta@gmail.com>
2024-04-04 09:12:24 +0100

    document data

--- R/data.R
+++ R/data.R
@@ -20,6 +20,11 @@
 #'   \item{symmetry}{Symmetry of the vowel system.}
 #'   \item{n_nonperipheral}{Number of non-peripheral vowels.}
 #'   \item{V1OS, V14F4}{Original symbol (OS), Alternative Interpretation (PS), first, second, third and fourth formant. Vowels are numbered within each language.}
+#'   \item{F1Centroid}{Mean F1.}
+#'   \item{F1CentroidPosition}{Scaled mean F1.}
+#'   \item{F1UCA}{Universal Centroid Approximation index for F1 (F1UCA = F1MIN + 0.391(F1MAX-F1MIN)).}
+#'   \item{F2Centroid}{Mean F2.}
+#'   \item{F2CentroidPosition}{Scaled mean F2.}
 #' }
 #' @source <http://phonetics.linguistics.ucla.edu/facilities/databases/databases.html>
 "beckerkristal2010_wide"
@@ -45,6 +50,12 @@
 #'   \item{n_peripheral}{Number of peripheral vowels.}
 #'   \item{symmetry}{Symmetry of the vowel system.}
 #'   \item{n_nonperipheral}{Number of non-peripheral vowels.}
+#'   \item{F1Centroid}{Mean F1.}
+#'   \item{F1CentroidPosition}{Scaled mean F1.}
+#'   \item{F1UCA}{Universal Centroid Approximation index for F1 (F1UCA = F1MIN + 0.391(F1MAX-F1MIN)).}
+#'   \item{F2Centroid}{Mean F2.}
+#'   \item{F2CentroidPosition}{Scaled mean F2.}
+#'   \item{F2UCA}{Universal Centroid Approximation index for 21 (F2UCA = F2MIN + 0.466(F2MAX-F2M1N)).}
 #'   \item{OS}{Original symbol.}
 #'   \item{PS}{Alternative symbol.}
 #'   \item{F1, F4}{Values of first, second, third and fourth formants.}
